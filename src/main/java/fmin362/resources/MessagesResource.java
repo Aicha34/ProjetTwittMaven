8ecd6588be48d0ebce5d69bd6b4efe3852ff78bf
Aicha34 <at-aicha@hotmail.com>
2012-11-22 18:40:32 +0100

    liste messages

--- src/main/java/fmin362/resources/MessagesResource.java
+++ src/main/java/fmin362/resources/MessagesResource.java
@@ -16,19 +16,23 @@ import fmin362.model.Message;
 public class MessagesResource {
 	@GET
 @Produces( MediaType.APPLICATION_JSON )
-public List<Message> listeMessage(){
+public String listeMessage(){
 
 List<Message> liste=new ArrayList<Message>();
 
-Message m1=new Message(1,"bonjour toufik", new Date());
-Message m2=new Message(1,"bonjour aicha", new Date(12/06/2012));
-Message m3=new Message(1,"bonjour ubuntu", new Date(8/11/2012));
+Message m1=new Message("bonjour toufik", new Date());
+Message m2=new Message("bonjour aicha", new Date(12/06/2012));
+Message m3=new Message("bonjour ubuntu", new Date(8/11/2012));
 
 liste.add(m1);
 liste.add(m2);
 liste.add(m3);
-return liste;
-
+String str="Message          Date";
+for (int i=0;i<liste.size();i++){
+	str+=str+liste.get(i).getLabel()+liste.get(i).getDate();
+	
+}
+return str;
 
 //TODO
 
