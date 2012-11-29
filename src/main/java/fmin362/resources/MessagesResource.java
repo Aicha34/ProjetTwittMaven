177bbf456380052a69ead03700db6380be44dbf5
Aicha34 <at-aicha@hotmail.com>
2012-11-29 12:42:42 +0100

    liste des messages

--- src/main/java/fmin362/resources/MessagesResource.java
+++ src/main/java/fmin362/resources/MessagesResource.java
@@ -13,30 +13,32 @@ import fmin362.model.Message;
 
 
 @Path( "/messages" )
-public class MessagesResource {
+public class MessagesResource 
+{
 	@GET
-@Produces( MediaType.APPLICATION_JSON )
-public String listeMessage(){
+	@Produces( MediaType.APPLICATION_JSON )
+	public String listeMessage()
+	{
 
-List<Message> liste=new ArrayList<Message>();
+		List<Message> liste=new ArrayList<Message>();
 
-Message m1=new Message("bonjour toufik", new Date());
-Message m2=new Message("bonjour aicha", new Date(12/06/2012));
-Message m3=new Message("bonjour ubuntu", new Date(8/11/2012));
+		Message m1=new Message("bonjour toufik", new Date());
+		Message m2=new Message("bonjour aicha", new Date(12/06/2012));
+		Message m3=new Message("bonjour ubuntu", new Date(8/11/2012));
 
-liste.add(m1);
-liste.add(m2);
-liste.add(m3);
-String str="Message          Date";
-for (int i=0;i<liste.size();i++){
-	str+=str+liste.get(i).getLabel()+liste.get(i).getDate();
+		liste.add(m1);
+		liste.add(m2);
+		liste.add(m3);
+		String str="Message          Date";
+		for (int i=0;i<liste.size();i++){
+			str+=str+liste.get(i).getLabel()+liste.get(i).getDate();
 	
-}
+		}
 return str;
 
-//TODO
 
 
-}
+
+	}
 
 }
